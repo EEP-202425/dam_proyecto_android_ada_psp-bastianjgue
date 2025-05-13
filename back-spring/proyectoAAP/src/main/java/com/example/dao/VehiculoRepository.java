d766adc7d03efba29e17a5eeaa40470489fcdea5
bastianjgue <bastian-jeria1@eep-igroup.com>
2025-05-13 11:10:41 +0200

    Empezamos de 0

--- back-spring/proyectoAAP/src/main/java/com/example/dao/VehiculoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.model.Vehiculo;
-
-public interface VehiculoRepository extends CrudRepository <Vehiculo, Integer>{
-	
-}
